9530ecaf4ff1cfc7462a3f4839899e69eaa87e5e
silvialorusso <silvia.lorusso90@gmail.com>
2019-11-25 09:29:55 +0100

    passo 3 dell'assistente firebase per l'autenticazione

--- Auth_firebase/app/src/main/java/com/example/auth_firebase/RegisterActivity.java
+++ Auth_firebase/app/src/main/java/com/example/auth_firebase/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -18,11 +19,27 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //verifica se l'utente esiste
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Toast.makeText(this, "Utente già loggato.", Toast.LENGTH_SHORT).show();
+
+        //aggiorna l'interfaccia utente
+        //updateUI(currentUser);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         mConfermaPassword = (EditText)findViewById(R.id.etRegPassConf);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void btnRegistratiClick(View view) {
@@ -37,17 +54,20 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(intent2);
     }
 
+    //il nome deve avere almeno 3 lettere
     private boolean nomeValido(String nome){
-        if (nome.length() > 3)
+        if (nome.length() > 2)
             return true;
         else
             return false;
     }
 
+    //l'email deve contenere il carattere @
     private boolean emailValida(String email){
         return email.contains("@");
     }
 
+    //la password deve avere 8 lettere
     private boolean pwValida(String password){
         String confermaPassword = mConfermaPassword.getText().toString();
         return confermaPassword.equals(password) && password.length() == 8;
